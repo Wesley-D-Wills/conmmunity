410879354eb0236d9a97b506703a486160c27b98
wx1655 <819269647@qq.com>
2020-10-28 21:51:40 +0800

    init repo

---
+++ src/test/java/com/wills/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wills.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
